10b657242ba82cfe91c63c8fb740acf75a3a0404
panzzer802 <jaimitojamonsalchicha@gmail.com>
2026-06-18 22:24:28 -0700

    Proyecto portafolio tareas

---
+++ Backend/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado correctamente a MongoDB"))
+  .catch(error => console.log("Error al conectar con MongoDB:", error));
+
+const tareaSchema = new mongoose.Schema({
+  id: Number,
+  titulo: String,
+  url: String
+});
+
+const Tarea = mongoose.model("Tarea", tareaSchema);
+
+app.get("/api/tareas", async (req, res) => {
+  try {
+    const tareas = await Tarea.find();
+    res.json(tareas);
+  } catch (error) {
+    res.status(500).json({ mensaje: "Error al obtener tareas" });
+  }
+});
+
+app.post("/api/tareas", async (req, res) => {
+  try {
+    const nuevaTarea = new Tarea(req.body);
+    await nuevaTarea.save();
+
+    res.json({
+      mensaje: "Tarea guardada correctamente",
+      tarea: nuevaTarea
+    });
+  } catch (error) {
+    res.status(500).json({ mensaje: "Error al guardar la tarea" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${process.env.PORT}`);
+});
---
+++ Frontend/script.js
@@ -0,0 +1,35 @@
+fetch("http://localhost:3000/api/tareas")
+
+.then(res=>res.json())
+
+.then(datos=>{
+
+    const contenedor=document.getElementById("contenedor");
+
+    datos.forEach(tarea=>{
+
+        contenedor.innerHTML +=`
+
+        <div class="tarjeta">
+
+            <h3>${tarea.titulo}</h3>
+
+            <a href="${tarea.url}" target="_blank">
+
+                Abrir tarea
+
+            </a>
+
+        </div>
+
+        `;
+
+    });
+
+})
+
+.catch(error=>{
+
+    console.log(error);
+
+});
